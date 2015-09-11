4ec6b363bacace1dc0dd70f571180e400a00a0dd
ubriela <hto@usc.edu>
2015-09-11 10:50:29 -0700

    share code

--- src/test/datasets/GenericProcessorTest.java
+++ src/test/datasets/GenericProcessorTest.java
@@ -1,8 +1,5 @@
 /*******************************************************************************
  * @ Year 2013
- * This is the source code of the following papers. 
- * 
- * 1) Geocrowd: A Server-Assigned Crowdsourcing Framework. Hien To, Leyla Kazemi, Cyrus Shahabi.
  * 
  * 
  * Please contact the author Hien To, ubriela@gmail.com if you have any question.
@@ -62,7 +59,7 @@ public class GenericProcessorTest {
 		ArrivalRateGenerator.time_instances_per_cycle = 7;
 		int instances = ArrivalRateGenerator.time_instances_per_cycle * 8;
 		ScalingDataProcessor scale = new ScalingDataProcessor(instances,
-				ArrivalRateEnum.CONSTANT, ArrivalRateEnum.COSINE, 500, 1000, "./res/dataset/worker/",
+				ArrivalRateEnum.CONSTANT, ArrivalRateEnum.ZIPFIAN, 500, 1000, "./res/dataset/worker/",
 				"./res/dataset/task/");
 	}
 	
